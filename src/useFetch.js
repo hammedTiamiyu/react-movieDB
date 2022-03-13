99a354830ccc2975128f0015e42e110a5036fa10
HammedTiamiyu <hammedtiamity18@gmail.com>
2022-03-13 23:04:51 +0100

    initial commit

---
+++ src/useFetch.js
@@ -0,0 +1,36 @@
+// This custom hook component was created to handle the two instances of data fetch in Context.js and singleMovie.js
+import React, {useState, useEffect} from "react";
+
+const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
+
+const useFetch = (urlParams) => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState({show: false, msg: ""})
+  const [data, setData] = useState(null)
+
+    const fetchMovies = async (url) => {
+        setIsLoading(true)
+        try {
+        const response = await fetch(url)
+        const data = await response.json()
+        // console.log(data);
+        if (data.Response === 'True') {
+        setData(data.Search || data)
+        setError({show: false, msg : ""})
+        } else {
+        setError({show:true, msg:data.Error})
+        }
+        setIsLoading(false)
+        } catch (error) {
+        console.log(error);
+        }
+   }
+
+   useEffect(() => {
+       fetchMovies(`${API_ENDPOINT}${urlParams}`)
+   },[urlParams])
+
+   return {isLoading, data, error}
+}
+
+export default useFetch
